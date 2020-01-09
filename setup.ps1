37b6f3082a099c280d5084628755b899c1f0384b
Jalal <jalal@superusers.dk>
2020-01-09 15:18:39 +0100

    ed

--- setup.ps1
+++ setup.ps1
@@ -2,9 +2,22 @@ function tf-init{
     terraform init
 }
 
-function tf-deploy {
+function tf-plan {
     terraform plan
+    <#
+    No changes. Infrastructure is up-to-date.
+
+    This means that Terraform did not detect any differences between your
+    configuration and real physical resources that exist. As a result, no
+    actions need to be performed.
+    #>
+}
+
+function tf-deploy {
     terraform apply
+    
+    $DeploymentProperties = terraform output -json | ConvertFrom-Json
+    $DeploymentProperties.website_hostname.value
 }
 
 function tf-clean {
